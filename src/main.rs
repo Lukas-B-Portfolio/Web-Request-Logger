7dc3ca4edb3dfe0f57fc5e81c66cfee3e1ed76af
Lukas <lukas.brockman@stud.uni-goettingen.de>
2023-10-08 20:58:32 +0200

    Initial Commit with basic logging to log.txt

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::{
+    io::{prelude::*},
+    net::{TcpListener, TcpStream},
+};
+//use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::BufReader;
+use chrono::prelude::*;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let mut log = OpenOptions::new()
+        .append(true)
+        .open("log.txt")
+        .expect("Could not open File.");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        log_request(&stream, &mut log);
+        response(stream);
+    }
+}
+
+fn response(mut stream: TcpStream) {
+    let status_line = "HTTP/1.1 200 OK";
+    let response =
+        format!("{status_line}\r\n");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn log_request(mut stream: &TcpStream, log: &mut File) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let client_ip = stream.local_addr().unwrap().to_string();
+
+    log.write("\n-------------------------------------------\n".as_bytes())
+        .expect("Failed to write to log.");
+    log.write("Received Request at:\t".as_bytes()).expect("Failed to write to log.");
+    log.write(timestamp().as_bytes()).expect("Failed to write Time to log.");
+    log.write("\nFrom IP:\t\t\t\t".as_bytes()).expect("Failed to write to log.");
+    log.write(client_ip.as_bytes()).expect("Failed to write IP to log.");
+    log.write("\nRequest:\n".as_bytes()).expect("Failed to write to log.");
+
+    for s in &http_request {
+        log.write(s.as_bytes()).expect("Failed to write part of the request to log.");
+        log.write("\n".as_bytes()).expect("Failed to write to log.");
+    }
+    println!("Request: {:#?}", http_request);
+}
+
+fn timestamp() -> String{
+    format!("{}", Local::now().format("%d/%m/%Y %H:%M:%S"))
+}
+
+/*
+struct HttpRequest {
+    start_line : StartLine,
+    headers : HashMap<String, String>,
+    // body
+}
+
+struct StartLine {
+    verb : Verb,
+    target : Target,
+    version : HTTPVersion
+}
+
+enum Verb {
+    GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH
+}
+enum Target {
+    AbsolutePath,
+    CompleteUrl,
+    AuthorityComponent,
+    AsteriskForm
+}
+enum HTTPVersion {
+    HTTP1_0,
+    HTTP1_1
+}
+*/
+
+
