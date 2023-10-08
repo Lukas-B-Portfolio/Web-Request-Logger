7bb2e780b86c373b1b469d9ed0d338786e631fa6
Lukas <lukas.brockman@stud.uni-goettingen.de>
2023-10-09 00:12:02 +0200

    Added .create(true) to log file declaration.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::{
-    io::{prelude::*},
+    io::prelude::*,
     net::{TcpListener, TcpStream},
 };
 //use std::collections::HashMap;
+use chrono::prelude::*;
 use std::fs::{File, OpenOptions};
 use std::io::BufReader;
-use chrono::prelude::*;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     let mut log = OpenOptions::new()
         .append(true)
+        .create(true)
         .open("log.txt")
         .expect("Could not open File.");
 
@@ -22,14 +23,15 @@ fn main() {
     }
 }
 
+/// Always sends 200 OK response
 fn response(mut stream: TcpStream) {
     let status_line = "HTTP/1.1 200 OK";
-    let response =
-        format!("{status_line}\r\n");
+    let response = format!("{status_line}\r\n");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
 
+/// Writes Time and Ip to log file as well as a List of all lines in the stream
 fn log_request(mut stream: &TcpStream, log: &mut File) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
@@ -42,20 +44,27 @@ fn log_request(mut stream: &TcpStream, log: &mut File) {
 
     log.write("\n-------------------------------------------\n".as_bytes())
         .expect("Failed to write to log.");
-    log.write("Received Request at:\t".as_bytes()).expect("Failed to write to log.");
-    log.write(timestamp().as_bytes()).expect("Failed to write Time to log.");
-    log.write("\nFrom IP:\t\t\t\t".as_bytes()).expect("Failed to write to log.");
-    log.write(client_ip.as_bytes()).expect("Failed to write IP to log.");
-    log.write("\nRequest:\n".as_bytes()).expect("Failed to write to log.");
+    log.write("Received Request at:\t".as_bytes())
+        .expect("Failed to write to log.");
+    log.write(timestamp().as_bytes())
+        .expect("Failed to write Time to log.");
+    log.write("\nFrom IP:\t\t\t\t".as_bytes())
+        .expect("Failed to write to log.");
+    log.write(client_ip.as_bytes())
+        .expect("Failed to write IP to log.");
+    log.write("\nRequest:\n".as_bytes())
+        .expect("Failed to write to log.");
 
     for s in &http_request {
-        log.write(s.as_bytes()).expect("Failed to write part of the request to log.");
+        log.write(s.as_bytes())
+            .expect("Failed to write part of the request to log.");
         log.write("\n".as_bytes()).expect("Failed to write to log.");
     }
     println!("Request: {:#?}", http_request);
 }
 
-fn timestamp() -> String{
+/// Returns current (local) timestamp as String formatted as: Day/Month/Year Hour:Minute:Second
+fn timestamp() -> String {
     format!("{}", Local::now().format("%d/%m/%Y %H:%M:%S"))
 }
 
@@ -86,5 +95,3 @@ enum HTTPVersion {
     HTTP1_1
 }
 */
-
-
